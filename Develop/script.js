6f3a98615fc0d94d9e3a4e5c33e54b5951bb09ac
pvlln <pv823@nyu.edu>
2023-03-08 08:38:38 -0500

    added updated readme file, finished js and html minor details, fixed bugs

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,26 @@
 $(function () {
+  // Store the main div in variable
   var timeBlock = $('.time-block');
+  // Add event listener and create function to save user inputs
   timeBlock.on('click', '.saveBtn', function(event){
+    // Target the id to identify the timestamp
     var hourId = $(this).parent('div').attr('id');
+    // Take the input by the user
     var userInput = $(this).parent().children().eq(1).val();
+    // Check in console
     console.log(hourId, userInput);
+    // Store in local storage
     localStorage.setItem(hourId, userInput);
   })
-
+  // Create variable with current dayjs info.
   var now = dayjs();
+  // Retrieve hour
   var currentHour = now.hour();
+  // Create array of timeslot elements in html
   var timeSlots = $('.time-slots').children();
+  // Console check
   console.log(currentHour, timeSlots);
+  // Loop that adds classes to the timeslots
   for (let i=0; i < timeSlots.length; i++){
     var timeStamp = $(timeSlots[i]).attr('id');
     var inputField = $(timeStamp).children('textarea');
@@ -23,18 +33,21 @@ $(function () {
     } else {
       $(timeSlots[i]).addClass('future');
     }
-
-    // NOT WORKING- REVISE
+    // forEach loop that iterates through all items in local storage
     Object.keys(localStorage).forEach(function(key){
+      // Considitional that checks if key matches a timestamp
       if (key == timeStamp){
         var item = localStorage.getItem(key);
-        $(inputField).text(item);
+        // Print value in timestamp
+        $(timeSlots[i]).children('textarea').text(item);
+        console.log(item);
+      }
+      else {
+        console.log("No match", key);
       }
     });
   }
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  // Add current date at the top of the page.
   var formattedDay = now.format('[Today is] MMM D, YYYY');
   $('#currentDay').text(formattedDay);
 });
--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # 05 Third-Party APIs: Work Day Scheduler
 
-## Your Task
+## Description
 
-Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+I created a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
 
 You'll need to use the [Day.js](https://day.js.org/en/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Day.js in the browser.
+
+## LINK TO DEPLOYED APPLICATION
+
+You can access my application from here:
+// Insert link
 
 ## User Story
 
@@ -34,8 +39,7 @@ THEN the saved events persist
 
 The following animation demonstrates the application functionality:
 
-<!-- @TODO: create ticket to review/update image) -->
-![A user clicks on slots on the color-coded calendar and edits the events.](./Assets/05-third-party-apis-homework-demo.gif)
+![A user clicks on slots on the color-coded calendar and edits the events.](./Assets/screenshot.png)
 
 ## Grading Requirements
 
